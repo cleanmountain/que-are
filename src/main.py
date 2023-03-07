92351ddd64d86623e2df97cf262d89435690e329
cleanmountain <kjaer_andreas@yahoo.dk>
2023-03-07 21:40:25 +0100

    minor changes

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
+from __future__ import annotations
 import cv2
 import csv
+import numpy as np
 from pyzbar.pyzbar import decode
 from dataclasses import dataclass
 
 
-FILEPATH: str = ""
+PATH: str = ""
+FILEPATH: str = PATH + ""
 
 
 @dataclass
@@ -15,20 +18,27 @@ class QrCode:
     
     # kinda the "main" method for now
     def decode_qr(self) -> None:
-        image_data: list = self.__read_image()
+        image_data: np.ndarray = self.__read_image()
+        decoded_image_data: list = self.__decode_image(image_data)
 
         # temp
-        self.__print_image_data(image_data)
+        self.__print_image_data(decoded_image_data)
+
+        return decoded_image_data
 
 
-    def __read_image(self) -> list:
+    def __read_image(self) -> np.ndarray:
         image = cv2.imread(self.image_path, 0)
-        image_data: list = decode(image)
+        return image
+    
+
+    def __decode_image(self, image_data: np.ndarray) -> list:
+        image_data: list = decode(image_data)
         return image_data
 
 
-    def __print_image_data(self, image_data: list) -> None:        
-        for data in image_data:
+    def __print_image_data(self, decoded_image_data: list) -> None:        
+        for data in decoded_image_data:
             line: str = data.data.decode("utf-8")
             print(line)
 
