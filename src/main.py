5285bdce42205ad50088aabc5648042b527aa1fc
cleanmountain <kjaer_andreas@yahoo.dk>
2023-03-07 20:13:43 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,37 @@
+import cv2
+import csv
+from pyzbar.pyzbar import decode
+from dataclasses import dataclass
+
+
+FILEPATH: str = "samplecsv-qr.png"
+
+@dataclass
+class QrCode:
+    """Class for QR code reading and decoding from image file"""
+    image_path: str
+
+    
+    # kinda the "main" method for now
+    def decode_qr(self) -> None:
+        image_data: list = self.__read_image()
+
+        # temp
+        self.__print_image_data(image_data)
+
+
+    def __read_image(self) -> list:
+        image = cv2.imread(self.image_path, 0)
+        image_data: list = decode(image)
+        return image_data
+
+
+    def __print_image_data(self, image_data: list) -> None:        
+        for data in image_data:
+            line: str = data.data.decode("utf-8")
+            print(line)
+
+
+if __name__ == "__main__":
+    qr = QrCode(FILEPATH)
+    qr.decode_qr()
